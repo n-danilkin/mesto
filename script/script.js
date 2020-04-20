ba4c42a9cb72a80784b4ae062f1eadade3c8ab31
ElF649 <danilkinnikita@ya.ru>
2020-04-20 21:13:52 +0300

    minor changes 2

--- script/script.js
+++ script/script.js
@@ -1,39 +1,40 @@
-let popup = document.querySelector('.popup');
-let formEdit = document.querySelector('.popup__form-edit');
-let inputProfileName = formEdit.querySelector('.popup__input-name');
-let inputProfileDescriprion = formEdit.querySelector('.popup__input-description');
-let profileSaveButton = formEdit.querySelector('.popup__save-button');
-let popupCloseButton = document.querySelector('.popup__close-button');
-let popupOpenButton = document.querySelector('.profile__info-edit-button');
-let profileName = document.querySelector('.profile__info-name');
-let profileDescription = document.querySelector('.profile__info-description');
+const popup = document.querySelector('.popup');
+const formEdit = document.querySelector('.popup__form-edit');
+const inputProfileName = formEdit.querySelector('.popup__input-name');
+const inputProfileDescriprion = formEdit.querySelector('.popup__input-description');
+const profileSaveButton = formEdit.querySelector('.popup__save-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const popupOpenButton = document.querySelector('.profile__info-edit-button');
+const profileName = document.querySelector('.profile__info-name');
+const profileDescription = document.querySelector('.profile__info-description');
 
-function inputProfileFiller () {    
+
+function inputProfileFiller() {
   inputProfileName.value = profileName.innerHTML;
   inputProfileDescriprion.value = profileDescription.innerHTML;
 }
 
-function saveProfile () {
+function saveProfile() {
   profileName.innerHTML = inputProfileName.value;
   profileDescription.innerHTML = inputProfileDescriprion.value;
-  closePopup ();
+  closePopup();
 }
 
-function closePopup () {
+function closePopup() {
   popup.style.display = 'none';
 }
 
-function openPopup () {
+function openPopup() {
   popup.style.display = 'block';
-  inputProfileFiller ();
+  inputProfileFiller();
 }
 
 
-document.querySelector('.popup__form-edit').addEventListener('submit', function(e) {
+formEdit.addEventListener('submit', function (e) {
   e.preventDefault();
 });
 
 profileSaveButton.addEventListener('click', saveProfile);
 popupCloseButton.addEventListener('click', closePopup);
 popupOpenButton.addEventListener('click', openPopup);
-inputProfileFiller ();
+inputProfileFiller();
